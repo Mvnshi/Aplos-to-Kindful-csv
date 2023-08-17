a4dc75c271f99a203a38177d97df6d12927c9671
Munshi <73901454+Mvnshi@users.noreply.github.com>
2023-08-16 20:13:58 -0400

    Added automatic sum calc & delete empty rows.

--- Sheet-Editor.js
+++ Sheet-Editor.js
@@ -1,5 +1,4 @@
-function DataModifier() 
-{
+function modifySheetAndDeleteChars() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
 
   // Delete the first 5 rows
@@ -13,10 +12,8 @@ function DataModifier()
   var lastRow = sheet.getLastRow();
   var rangeD = sheet.getRange("D1:D" + lastRow);
   var valuesD = rangeD.getValues();
-  for (var i = valuesD.length - 1; i >= 0; i--) 
-  {
-    if (valuesD[i][0].toString().toLowerCase().includes("stripe")) 
-    {
+  for (var i = valuesD.length - 1; i >= 0; i--) {
+    if (valuesD[i][0].toString().toLowerCase().includes("stripe")) {
       sheet.deleteRow(i+1);  // i+1 because array index is 0-based, but sheet row is 1-based
     }
   }
@@ -31,10 +28,8 @@ function DataModifier()
   // Replace "General Fund" with "General Operating" in column G
   var rangeG = sheet.getRange("G2:G" + sheet.getLastRow());
   var valuesG = rangeG.getValues();
-  for (var i = 0; i < valuesG.length; i++) 
-  {
-    if (valuesG[i][0] === "General Fund") 
-    {
+  for (var i = 0; i < valuesG.length; i++) {
+    if (valuesG[i][0] === "General Fund") {
       valuesG[i][0] = "General Operating";
     }
   }
@@ -50,8 +45,7 @@ function DataModifier()
     var cellValue = valuesF[i][0];
 
     // Check that the cell is not empty
-    if (cellValue !== '') 
-    {
+    if (cellValue !== '') {
       // Remove the first 7 characters from the cell value
       var updatedCellValue = cellValue.toString().substring(7);
 
@@ -78,9 +72,7 @@ function DataModifier()
   var rangeM = sheet.getRange("M1:M" + sheet.getLastRow());
   var valuesM = rangeM.getValues();
 
-  rangeM.setNumberFormat("@");
-
-  // Check all values
+  // Update to correct zip code format
   for (var i = 0; i < valuesM.length; i++) 
   {
     // Get current cell value
@@ -91,10 +83,47 @@ function DataModifier()
     {
       // Add '0' to the beginning of the number
       var updatedCellValue = '0' + cellValue;
+
       // Update the cell value
       valuesM[i][0] = updatedCellValue;
     }
   }
+  
   // Update column M with new values
   rangeM.setValues(valuesM);
+  if (!verifyColumnHeaders(sheet)) 
+  {
+    throw new Error("Column headers verification failed. Please check the logs.");
+  }
+
+  //Delete Empty Columns
+  var firstColRange = sheet.getRange("A1:A" + sheet.getLastRow());
+  var firstColValues = firstColRange.getValues();
+  for (var i = firstColValues.length - 1; i >= 0; i--) 
+  {
+    if (!firstColValues[i][0]) 
+    {
+        sheet.deleteRow(i + 1);
+    }
+  }
+  
+  // Calculate Correct Total
+  var lastRowInP = sheet.getLastRow();
+  var cellP = sheet.getRange("P" + (lastRowInP + 0));
+  cellP.setFormula("=SUM(P1:P" + (lastRowInP - 1) + ")");
+}
+
+// Verify column headers
+function verifyColumnHeaders(sheet) 
+{
+  var expectedHeaders = ["Name", "Organization", "Email", "Date", "Transaction Type", "Check Number", "Fund", "Campaign", "Address", "Address2", "City", "State", "Postal Code", "Country", "Phone", "Amount"];
+  var actualHeaders = sheet.getRange("A1:P1").getValues()[0]; // Get the first row, which is the header
+
+  for (var i = 0; i < expectedHeaders.length; i++) {
+    if (actualHeaders[i] !== expectedHeaders[i]) {
+      Logger.log("Error: Expected header for column " + (i + 1) + " to be '" + expectedHeaders[i] + "' but found '" + actualHeaders[i] + "'.");
+      return false; // Or throw an exception if you prefer
+    }
+  }
+  return true;
 }
